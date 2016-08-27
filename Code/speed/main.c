1f93736ac58d147bd2bc10fbb5586af03bd499dc
MikeCDimmick <mike.c.dimmick@gmail.com>
2016-08-27 01:57:55 -0400

    Add files via upload

--- Code/speed/main.c
+++ Code/speed/main.c
@@ -13,34 +13,46 @@
 void setDisplay(int displayNum, int number);
 unsigned int count(unsigned int i);
 void processThreeDigitNumber(int number);
-void getReadings();
+int* getReadings();
 int processSpeed();
+
+//data to the displays are sent from PORT B
+#define SEVEN_SEG_DISPLAY_PORT PORTB
+#define SEVEN_SEG_DISPLAY_DDR  DDRB
+#define SEVEN_SEG_DATA_PINS  0x7F
+
+#define SEVEN_SEG_CONTROL_PORT PORTD
+#define SEVEN_SEG_CONTROL_DDR  DDRD
+#define SEVEN_SEG_CONTROL_PINS  0x07
+
+#define MINIMUM_DISPLAY_SPEED 10
 
 #define DISPLAY_TIME 1000	//number proportional to a time that the speed shows up for
 #define SEGMENT_PAUSE 500	//time between segment switches in us
-#define NUM_READINGS 10		//number of samples to take from the sensor
-#define READ_TIME 3000		//total time that program is pulling data from sensor (READ_TIME/NUM_READINGS is the sample period)
+#define POSITION_SAMPLES 10		//number of samples to take from the sensor
+#define SPEED_SAMPLES 8
+#define READ_TIME 3000		//total time that program is pulling data from sensor (READ_TIME/POSITION_SAMPLES is the sample period)
 
-int readings[NUM_READINGS];	//array to hold the samples from the sensor, these values are in cm 
-int speeds[NUM_READINGS - 2]; //array that holds the speed calculations
 
 int main(void)
 {
-	DDRB = 0xFF; //PORTB as Output for 7-segment LEDs
-	DDRD = 0x07; //PORTD as Output for control, PD0 is digit 1, PD1 is digit 2, PD2 is digit 3
+	SEVEN_SEG_DISPLAY_DDR = SEVEN_SEG_DATA_PINS; //PORTB as Output for 7-segment LEDs
+	SEVEN_SEG_CONTROL_DDR = SEVEN_SEG_CONTROL_PINS; //PORTD as Output for control, PD0 is digit 1, PD1 is digit 2, PD2 is digit 3
 	
 	int speed = 0;
+	int *positions;
 	while(1) //infinite loop, take sensor readings, calculate average speed, display 3 digit number
 	{
-		 getReadings();
-		 speed = processSpeed();
-		 if(speed > 10)
-			processThreeDigitNumber(speed);
+		 positions = getReadings();
+		 speed = processSpeed(positions);
+		 if(speed > MINIMUM_DISPLAY_SPEED)
+			 processThreeDigitNumber(speed);
+
 	}
 }
 
 //count the number of digits in a number
-unsigned int count(unsigned int i) 
+unsigned int count(unsigned int i)
 {
 	unsigned int ret=1;
 	while (i/=10) ret++;
@@ -59,6 +71,7 @@ void processThreeDigitNumber(int number)
 		arr[dig]=number%10;
 		number/=10;
 	}
+	
 	//arr has the following contents: [digit 3, digit 2, digit 1]
 	//if there are three digits then we can just send out the array
 	if(compare == 3)
@@ -142,30 +155,34 @@ void setDisplay(int displayNum, int number)
 		PORTB = 0x98;
 }
 
-//pulls NUM_READINGS data points from the sensor, places values in an array 
-void getReadings()
+//pulls POSITION_SAMPLES data points from the sensor, places values in an array 
+int* getReadings()
 {
-	for(int i = 0; i < NUM_READINGS; i++)
+	static int positions[POSITION_SAMPLES];	//array to hold the samples from the sensor, these values are in cm
+	
+	for(int i = 0; i < POSITION_SAMPLES; i++)
 	{
-		readings[i] = read_sonar();
-		_delay_ms(READ_TIME/NUM_READINGS);
+		positions[i] = read_sonar();
+		_delay_ms(READ_TIME/POSITION_SAMPLES);
 	}
+	return positions;
 }
 
 //calculates speeds from the distance readings using the centered difference method (dx/dt = (x_n+1 - x_n-1)/2*deltaT)
-int processSpeed()
+int processSpeed(int* positions)
 {
-	//calculate centered difference and put speed into an array
-	for(int i = 0; i < NUM_READINGS-2; i++)
-		speeds[i] = abs((readings[i+2]-readings[i])/(2*(READ_TIME/NUM_READINGS)/1000));
+	float speeds[SPEED_SAMPLES];
+	float sum = 0;
 
-	//sum together the speeds
-	int sum = 0;
-	for(int i = 0; i < NUM_READINGS-2; i++)
-			sum += speeds[i];
+	//calculate centered difference and put speed into an array
+	for(int i = 0; i < SPEED_SAMPLES; i++)
+	{
+		speeds[i] = abs((positions[i+2]-positions[i])/(2*(READ_TIME/POSITION_SAMPLES)/1000.0));
+		sum += speeds[i];
+	}
 			
-	float avgSpeed = sum/(NUM_READINGS-2);	//calculate average speed in cm/s
+	float avgSpeed = sum/(SPEED_SAMPLES);	//calculate average speed in cm/s
 	avgSpeed = avgSpeed*0.02237;			//convert speed from cm/s to MPH
-	avgSpeed = avgSpeed*87					//scale up from HO to real life
+	avgSpeed = avgSpeed*87;					//scale up from HO to real life
 	return (int)avgSpeed;					
 }
